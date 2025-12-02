9c003e46097f094af25df4556622cde30e28e9e1
drive <driveasano@gmail.com>
2025-12-02 11:13:03 +0900

    feat: Add new Astro-based docs and Next.js moc1 applications, and update project repository URLs.

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -1,26 +1,55 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightLinksValidator from "starlight-links-validator";
+import starlightImageZoom from "starlight-image-zoom";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    site: "https://reflenge.github.io",
+    base: "/mobi-pita/docs",
+    integrations: [
+        starlight({
+            plugins: [starlightLinksValidator(), starlightImageZoom()],
+            favicon: "/favicon.ico",
+            title: "Mobi-Pita",
+            locales: {
+                root: {
+                    label: "日本語",
+                    lang: "ja",
+                },
+                en: {
+                    label: "English",
+                    lang: "en",
+                },
+            },
+            social: [
+                {
+                    label: "GitHub",
+                    icon: "github",
+                    href: "https://github.com/reflenge/mobi-pita",
+                },
+                {
+                    label: "moc1",
+                    icon: "puzzle",
+                    href: "https://reflenge.github.io/mobi-pita/moc1",
+                },
+            ],
+            sidebar: [
+                {
+                    label: "Guides",
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        { label: "Example Guide", slug: "guides/example" },
+                    ],
+                },
+                {
+                    label: "Reference",
+                    autogenerate: { directory: "reference" },
+                },
+            ],
+        }),
+        sitemap(),
+    ],
 });
--- apps/moc1/next.config.ts
+++ apps/moc1/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  basePath: "/Mobi-Pita/moc1",
+  basePath: "/mobi-pita/moc1",
   output: "export",
 };
 
